d4544e14f81ff38b780d51f07227c506fd9afdb0
Xavier Heugue <xavier.heugue@gmail.com>
2015-12-15 11:48:58 +0100

    Ajout d'un fichier de configuration contenant des constantes pour naviguer plus facilement dans le projet

---
+++ application/config.php
@@ -0,0 +1,15 @@
+<?php
+    define("MODEL_PATH", "application/model/");
+    define("CONTROLER_PATH", "application/controler/");
+    define("VIEW_PATH", "application/view/");
+
+    define("CSS_PATH", "resource/css/");
+    define("JS_PATH", "resource/js/");
+    define("IMAGE_PATH", "resource/img");
+
+    define("DSN", "mysql:dbname=");
+    define("DB_NAME", "");
+    define("DB_USER", "");
+    define("DB_PASSWORD", "");
+
+?>
