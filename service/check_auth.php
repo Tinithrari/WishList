b2f51d4dadf7597cfbe0bce3bd889b0f36d690fe
agathe <blotagathe@gmail.com>
2016-01-05 16:13:28 +0100

    check_auth fait

--- service/check_auth.php
+++ service/check_auth.php
@@ -5,11 +5,13 @@
  * Date: 05/01/16
  * Time: 15:03
  */
-
+    include_once (MODEL_PATH . "utilisateur.class.php");
     include_once (CONTROLER_PATH . "UtilisateurControleur.class.php");
 
-    session_start();
-    if !isset($_SESSION["username"]) or !issset($_SESSION["id"]) or !isset($_SESSION["password"]){
-        header("Location : ../index.php");
-
+    if (!isset($_SESSION["username"]) or !isset($_SESSION["id"]) or !isset($_SESSION["password"])) {
+        $controller = new \controler\UtilisateurControler();
+        $usr=$controller->trouverUtilisateurParId($_SESSION["id"]);
+        if ($usr == NULL || $usr->pseudo != $_SESSION["username"] || $usr->mdp != $_SESSION['password'] )
+            header("Location : ../index.php");
+    }
 ?>
